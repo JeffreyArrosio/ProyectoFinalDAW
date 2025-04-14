82659d98c4fe41cdd32adb5f4298938e917e3f85
JeffreyArrosio <jeffrey.aj98@gmail.com>
2025-04-14 11:22:15 +0300

    Las URL siempre redirigen al mismo destino, poner URL del front cuando se implemente este

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return ['Laravel' => app()->version()];
+// });
+
+Route::get('/{any}', function () {
     return ['Laravel' => app()->version()];
-});
+})->where('any', '.*');;
 
 require __DIR__.'/auth.php';
