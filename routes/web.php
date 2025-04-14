384174a37244ae3f7fe5df13d1b5843a6051c8dc
JeffreyArrosio <jeffrey.aj98@gmail.com>
2025-04-14 11:38:44 +0300

    permitiendo API y otras rutas

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{any}', function () {
     return ['Laravel' => app()->version()];
-})->where('any', '.*');;
+})->where('any', '^(?!api|storage|login|register).*$');;
 
 require __DIR__.'/auth.php';
