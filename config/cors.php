95ce71dedcb5cff5dac759365fa6d2f47513252b
Danny79k <belloli.danny@gmail.com>
2025-05-12 14:37:26 +0300

    login VPS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
         'allowed_methods' => ['*'],
 
-        'allowed_origins' => ['https://rafael.informaticamajada.es/'],
+        'allowed_origins' => ['https://rafael.informaticamajada.es'],
 
         'allowed_origins_patterns' => [],
 
