a0e974d8a57b284fe876668959eab10d2cf994a0
Danny79k <belloli.danny@gmail.com>
2025-05-14 10:43:54 +0300

    disabled pagination on categories

--- app/Http/Controllers/Api/v1/ModelController/CategoryController.php
+++ app/Http/Controllers/Api/v1/ModelController/CategoryController.php
@@ -6,10 +6,11 @@ use App\Models\Category;
 use Orion\Concerns\DisableAuthorization;
 use Orion\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Orion\Concerns\DisablePagination;
 
 class CategoryController extends Controller
 {
-    use DisableAuthorization;
+    use DisableAuthorization, DisablePagination;
 
     protected $model = Category::class;
 }
