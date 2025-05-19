5f8e259bf5643937ec9b65993306af709f16cac3
Danny79k <belloli.danny@gmail.com>
2025-05-20 01:35:34 +0300

    add HasApiTokens trait to FollowController for API token management

--- app/Http/Controllers/Api/v1/ModelController/FollowController.php
+++ app/Http/Controllers/Api/v1/ModelController/FollowController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Api\v1\ModelController;
 
 use App\Models\Follow;
+use Laravel\Sanctum\HasApiTokens;
 use Orion\Concerns\DisableAuthorization;
 use Orion\Http\Controllers\Controller;
 class FollowController extends Controller
 {
-    use DisableAuthorization;
+    use DisableAuthorization, HasApiTokens;
     protected $model = Follow::class;
 
 }
