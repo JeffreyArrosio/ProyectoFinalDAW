30798858c2f3c40d4247bdaf4f36f94c0c6485e6
Danny79k <belloli.danny@gmail.com>
2025-05-19 19:22:02 +0300

    add DisableAuthorization trait to UserFollowersController

--- app/Http/Controllers/Api/v1/RelationController/UserFollowersController.php
+++ app/Http/Controllers/Api/v1/RelationController/UserFollowersController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Api\v1\RelationController;
 
 use App\Models\User;
+use Orion\Concerns\DisableAuthorization;
 use Orion\Http\Controllers\RelationController;
 use Illuminate\Http\Request;
 
 class UserFollowersController extends RelationController
 {
+
+    use DisableAuthorization;
     protected $model = User::class;
 
     protected $relation = 'followers';
