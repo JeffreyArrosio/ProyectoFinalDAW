3235e46b35df2264ca2561a945172eef7e9cdf32
Danny79k <belloli.danny@gmail.com>
2025-05-19 18:55:51 +0300

    add DisableAuthorization trait to FollowerRelationController and NewsColumnsController

--- app/Http/Controllers/Api/v1/RelationController/FollowerRelationController.php
+++ app/Http/Controllers/Api/v1/RelationController/FollowerRelationController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers\Api\v1\RelationController;
 
+use Orion\Concerns\DisableAuthorization;
 use Orion\Http\Controllers\RelationController;
 use App\Models\Follow;
 
 class FollowerRelationController extends RelationController
 {
+
+    use DisableAuthorization;
     protected $model = Follow::class;
 
     protected $relation = 'follower';
--- app/Http/Controllers/Api/v1/RelationController/NewsColumnsController.php
+++ app/Http/Controllers/Api/v1/RelationController/NewsColumnsController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Api\v1\RelationController;
 
 use App\Models\News;
+use Orion\Concerns\DisableAuthorization;
 use Orion\Http\Controllers\RelationController;
 use Illuminate\Http\Request;
 
 class NewsColumnsController extends RelationController
 {
+
+    use DisableAuthorization;
     protected $model = News::class;
 
     protected $relation = 'columns';
