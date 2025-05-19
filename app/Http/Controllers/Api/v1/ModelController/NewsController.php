332a3292acb310116f087fbf4e4f79e27ba3bbd0
JeffreyArrosio <jeffrey.aj98@gmail.com>
2025-05-20 01:39:38 +0300

    Reaciando store

--- app/Http/Controllers/Api/v1/ModelController/NewsController.php
+++ app/Http/Controllers/Api/v1/ModelController/NewsController.php
@@ -17,30 +17,27 @@ class NewsController extends Controller
     protected $model = News::class;
     protected $policy = NewsPolicy::class;
 
-    public function store(Request $request){
-
-        $validatedData = $request->validate([
-            'main_image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:4096',
+    public function store(Request $request)
+    {
+        $request->validate([
+            'main_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:4096',
         ]);
-        $news = new News();
-        $news->title = $request['title'];
-        $news->content = $request['content'];
-        $news->date = $request['date'];
-        $news->type= $request['type'];
-        $news->urgent = $request['urgent'] ?? 0;
-        $news->premium = $request['premium'] ?? 0;
-        $news->user_id = $request['user_id'];
-        $news->category_id = $request['category_id'];
 
-        if ($request->hasFile('main_image')) {
-            $path = $request->file('main_image')->store('news', 'public');
-            $news->main_image = $path;
-        }else{
-            $news->main_image = "Enlace";
-        }
+        $news = new News();
+        $news->title = $request->title;
+        $news->content = $request->content;
+        $news->date = $request->date;
+        $news->type = $request->type;
+        $news->urgent = $request->urgent ?? 0;
+        $news->premium = $request->premium ?? 0;
+        $news->user_id = $request->user_id;
+        $news->category_id = $request->category_id;
+
+        $path = $request->file('main_image')->store('news', 'public');
+        $news->main_image = $path;
 
         $news->save();
-        return response()->noContent();
+
+        return response()->json($news, 201);
     }
 }
-
