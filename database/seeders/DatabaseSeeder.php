ce43837e02369d477c054e88d8758b2e1c2ae1ba
JeffreyArrosio <jeffrey.aj98@gmail.com>
2025-05-20 21:43:02 +0300

    Admin se crea en el seeder directamente

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 use function Pest\Laravel\call;
 
@@ -16,8 +17,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::firstOrCreate(
+            ['email' => 'admin@admin.com'], 
+            [ 
+                'name' => 'Admin',
+                'password' => bcrypt('admin1234'),
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+                'type' => "writer",
+                'admin' => 1,
+            ]
+        );
         User::factory(20)->create();
-
         $this->call([
             CategorySeeder::class,
             NewsSeeder::class,
