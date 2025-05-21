c705580f8e86cd22eb317dd86d7d635edea304fd
Danny79k <belloli.danny@gmail.com>
2025-05-21 17:46:58 +0300

    add DisablePagination trait to NewsUserController

--- app/Http/Controllers/Api/v1/RelationController/NewsUserController.php
+++ app/Http/Controllers/Api/v1/RelationController/NewsUserController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Api\v1\RelationController;
 
 use App\Models\News;
+use Orion\Concerns\DisablePagination;
 use Orion\Http\Controllers\RelationController;
 use Illuminate\Http\Request;
 
 class NewsUserController extends RelationController
 {
+
+    use DisablePagination;
     protected $model = News::class;
 
     protected $relation = 'user';
