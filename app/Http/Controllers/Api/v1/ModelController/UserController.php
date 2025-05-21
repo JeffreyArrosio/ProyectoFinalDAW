de829c2663f507e56a55502180396f48b6e60c0f
Danny79k <belloli.danny@gmail.com>
2025-05-21 18:11:45 +0300

    add HasApiTokens trait to UserController for API token support

--- app/Http/Controllers/Api/v1/ModelController/UserController.php
+++ app/Http/Controllers/Api/v1/ModelController/UserController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Api\v1\ModelController;
 
 use App\Models\User;
 use App\Policies\UserPolicy;
+use Laravel\Sanctum\HasApiTokens;
 use Orion\Concerns\DisableAuthorization;
 use Orion\Http\Controllers\Controller;
 use Orion\Concerns\DisablePagination;
 
 class UserController extends Controller
 {
-    use DisableAuthorization, DisablePagination;
+    use HasApiTokens, DisablePagination;
 
     protected $model = User::class;
     protected $policy = UserPolicy::class;
